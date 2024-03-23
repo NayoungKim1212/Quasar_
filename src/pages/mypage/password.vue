3e16f640bd55c6fa807b767dc84d959d5dc2af59
NayoungKim1212 <kkd04250@naver.com>
2024-03-23 23:15:09 +0900

    posts

---
+++ src/pages/mypage/password.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="text-h4">Mypage</div>
+        <q-layout>
+            <ul>
+                <li>
+                    <router-link to="/profile">Profile</router-link>
+                </li>
+                <li>
+                    <router-link to="/password">Password</router-link>
+                </li>
+                <li>
+                    <router-link to="/bookmark">Bookmark</router-link>
+                </li>
+            </ul>
+            <q-page-container>
+                <router-view />
+            </q-page-container>
+        </q-layout>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
