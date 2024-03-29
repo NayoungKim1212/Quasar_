35c6fba02401e64805cec200d1434d60415690e8
NayoungKim1212 <kkd04250@naver.com>
2024-03-29 12:10:07 +0900

    feat 커뮤니티 목록 리스트 출력

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
-    <q-layout view="hHh lpR fFf">
-
+    <q-layout view="hhh lpR fFf" class="bg-grey-2">
         <q-header bordered class="bg-white text-grey-9">
             <q-toolbar>
-                <q-btn flat dense> <!--그림자를 제거하기 위해 flat, 버튼을 조금 더 얇게 만들기 위해 dense-->
+                <q-btn flat dense to="/"> <!--그림자를 제거하기 위해 flat, 버튼을 조금 더 얇게 만들기 위해 dense-->
                     <q-toolbar-title>
                         <q-avatar>
                             <img src="/logo.png"> <!--이미지 경로 : public 아래에 위치-->
@@ -13,7 +12,7 @@
                 </q-btn>
                 <q-space /> <!--버튼에 의해 컴포넌트가 한 쪽으로 쏠렸을 경우 사용-->
                 <q-btn stretch flat label="장보기" href="https://shopping.naver.com/market/emart/home" target="_blank" />
-                <!---외부 링크로 연결 시 새 창을 띄워 보내기 위해 target="_blank" 사용--> 
+                <!---외부 링크로 연결 시 새 창을 띄워 보내기 위해 target="_blank" 사용-->
                 <q-separator class="q-my-md q-mr-md" vertical />
                 <q-btn unelvaled rounded color="primary" label="로그인 / 회원가입" />
                 <!---그림자 제거를 위해 unelvaled, 둥글게 표현하기 위해 rounded-->
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
+    <q-page padding>
         <div class="text-h4">커뮤니티 목록</div>
-        <ul>
-            <li>
-                <router-link to="/posts/1">1번 게시글</router-link>
-            </li>
-        </ul>
-    </div>
-
+        <section class="q-qutter-y-sm q-mt-lg">
+            <q-card v-for="id in 100" :key="id"> <!--1부터 20까지 순회, 각 id에 대해 <q-card> 컴포넌트를 생성-->
+                <router-link :to="`/posts/${id}`">
+                    <q-card-section>
+                        {{ id }}게시글</q-card-section>
+                </router-link>
+            </q-card>
+        </section>
+    </q-page>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 </script>
 
