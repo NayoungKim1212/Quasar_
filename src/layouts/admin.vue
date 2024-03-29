f50d33b96cfec19640c10a073661126223f8eaf6
NayoungKim1212 <kkd04250@naver.com>
2024-03-29 09:16:38 +0900

    comment

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-<div class="text-h2">admin Layout</div>
-<router-view />
+        <div class="text-h2">admin Layout</div>
+        <router-view /> <!--디렉터리 안에있는 컴포넌트들을 렌더링 하기 위해서 사용-->
     </div>
 </template>
 
@@ -9,6 +9,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
